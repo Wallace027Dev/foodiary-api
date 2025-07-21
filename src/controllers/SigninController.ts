52f9309e2c982435846b9eb28af334e0598d20dd
Wallace027Dev <wallaceofc@hotmail.com>
2025-07-21 11:14:13 -0300

    validacao dos campos

--- src/controllers/SigninController.ts
+++ src/controllers/SigninController.ts
@@ -1,8 +1,12 @@
 import { HttpRequest, HttpResponse } from "../types/http";
 import { ok } from "../utils/http";
+import { z } from "zod";
+
+const schema = z.object({ email: z.email(), password: z.string().min(8) });
 
 export class SignInController {
   static async handle(request: HttpRequest): Promise<HttpResponse> {
     return ok({ accessCode: "Token de acesso..." });
   }
+
 }
--- src/controllers/SignupController.ts
+++ src/controllers/SignupController.ts
@@ -1,8 +1,29 @@
 import { HttpRequest, HttpResponse } from "../types/http";
-import { created } from "../utils/http";
+import { badRequest, created } from "../utils/http";
+import { z } from "zod";
+
+const schema = z.object({
+  goal: z.enum(["lose", "maintain", "gain"]),
+  gender: z.enum(["male", "female"]),
+  birthDate: z.iso.date(),
+  height: z.number().int().positive(),
+  weight: z.number().int().positive(),
+  activityLevel: z.number().min(1).max(5),
+  account: z.object({
+    name: z.string().min(3),
+    email: z.email(),
+    password: z.string().min(8),
+  }),
+});
 
 export class SignUpController {
   static async handle(request: HttpRequest): Promise<HttpResponse> {
-    return created({ accessCode: "Token de acesso..." });
+    const { success, error, data } = schema.safeParse(request.body);
+
+    if (!success) {
+      return badRequest({ error: error.issues });
+    }
+
+    return created({ data });
   }
 }
